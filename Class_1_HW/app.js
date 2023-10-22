81dc2917a3503f441f38a2d7c7e1afa1171cd960
imtiazalirb <imtiazali4090@gmail.com>
2023-10-22 19:21:49 +0600

    fix: removed array render and implemented rendering over single container

--- Class_1_HW/app.js
+++ Class_1_HW/app.js
@@ -4,24 +4,23 @@ const parent = React.createElement("div", {className: "parent"},
     [
         React.createElement("h1", {}, "Hello World!!"),
         React.createElement("p", {}, "This is pera!"),
-    ]
-)
+    ],
 
-const wapper = React.createElement("div", {className: "wapper"},
-     parent2 = React.createElement("div", {className: "wapper-parent"},
+     wapper = React.createElement("div", {className: "wapper"},
+        parent2 = React.createElement("div", {className: "wapper-parent"},
      [
         React.createElement("h1", {className: "parent-title"}, "I am Parent."),
         React.createElement("p", {}, "Barir Korta"),
-     ]
-)
-)
+     ],
 
-const parent_two = React.createElement("div", {className: "parent2"},
+     parent_two = React.createElement("div", {className: "parent2"},
     [
         React.createElement("h1", {className: "parent-title2"}, "I am parent two."),
         React.createElement("p", {}, "Barir second Korta"),
     ]
 )
+)
+)
+)
 
-
-root.render([parent, wapper, parent_two]);
+root.render(parent);
